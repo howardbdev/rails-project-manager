063c0cad2e07f7d8a4f2f5a8624a57fb3aeeb307
Howard DeVennish <howarddevennish@gmail.com>
2017-05-28 16:23:07 +0000

    generate resource project and run migration

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,4 @@
 class User < ApplicationRecord
+  has_secure_password
+  enum role: [:worker, :supervisor, :admin]
 end
---
+++ spec/factories/projects.rb
@@ -0,0 +1,7 @@
+FactoryGirl.define do
+  factory :project do
+    name "MyString"
+    desription "MyString"
+    location "MyString"
+  end
+end
