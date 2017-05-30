bf7691cb9e59c829e9941afeac9d0d43539c3a83
Howard DeVennish <howarddevennish@gmail.com>
2017-05-30 18:03:55 +0000

    add new user and session views, update application layout

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,12 @@
 Rails.application.routes.draw do
   root 'application#hello'
-  resources :sessions
   resources :assignments
   resources :projects
   resources :users
+  get '/login', to: 'sessions#new'
+  post 'login', to: 'sessions#create'
+  post '/logout', to: 'sessions#destroy'
+  get '/signup', to: 'users#new'
+  post '/signup', to: 'users#create'
 
 end
