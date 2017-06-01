0abfdb09b38a772937e21096c98ff605f975edd8
Howard DeVennish <howarddevennish@gmail.com>
2017-06-01 13:20:16 +0000

    clean up comments in users controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -8,17 +8,14 @@ class UsersController < ApplicationController
 
   def create
     @user = User.find_by(email: user_params[:email])
-    if @user #that email was already found in database
-      # flash alert that user email is already in use
+    if @user
       flash[:alert] = "Error: User already exists with that email.  Check form or log in."
       redirect_to root_path
     else
       @user = User.new(user_params)
       if @user.save
-        # user was successfully created so redirect to show page
         redirect_to @user
       else
-        # show error and render signup page
         flash[:alert] = "Error: User not created"
         flash[:error] = @user.errors.full_messages.to_sentence
         render :new
