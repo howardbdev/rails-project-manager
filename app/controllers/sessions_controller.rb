b7b7430437fa6916d8f28a77d1ad70d790ea2308
Howard DeVennish <howarddevennish@gmail.com>
2017-06-01 13:49:16 +0000

    fix log in for sessions controller

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,12 +1,28 @@
 class SessionsController < ApplicationController
 
-  def create
+  def new
 
+  end
+
+  def create
+    @user = User.find_by(email: user_params[:email])
+    if @user && @user.authenticate(user_params[:password])
+      session[:user_id] = @user.id
+      redirect_to @user
+    else
+      render :new
+    end
   end
 
   def destroy
     session.clear
-    redirect_to root_path
+    redirect_to login_path
+  end
+
+  private
+
+  def user_params
+    params.permit(:name, :email, :password, :role)
   end
 
 end
