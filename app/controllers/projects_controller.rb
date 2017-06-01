0ef4b9ec582b98fcf791673fdef5ae7944bfc5f7
Howard DeVennish <howarddevennish@gmail.com>
2017-06-01 15:38:47 +0000

    stub out projects controller and views

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -1,3 +1,39 @@
 class ProjectsController < ApplicationController
   before_action :require_authentication
+  before_action :get_project, only: [:edit, :update, :destroy]
+
+  def index
+    @projects = Project.all
+  end
+
+  def new
+    @project = Project.new
+  end
+
+  def create
+
+  end
+
+  def edit
+
+  end
+
+  def update
+
+  end
+
+  def destroy
+    @project.delete
+    redirect to projects_path
+  end
+
+  private
+
+  def project_params
+    params.require(:project).permit(:name, :location, :description, :status, :owner_id)
+  end
+
+  def get_project
+    @project = Project.find_by(id: params[:id])
+  end
 end
