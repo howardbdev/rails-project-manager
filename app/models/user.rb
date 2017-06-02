903d6362e4a0676b4c6f34a9cbd2c57b177a3aca
Howard DeVennish <howarddevennish@gmail.com>
2017-06-02 14:14:28 +0000

    add dependent destroy to user assignments

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,7 +2,7 @@ class User < ApplicationRecord
   validates :name, :email, presence: true
   validates :email, uniqueness: true
   has_many :pet_projects, class_name: "Project", foreign_key: "owner_id"
-  has_many :assignments
+  has_many :assignments, dependent: :destroy
   has_many :projects, through: :assignments
   has_secure_password
   enum role: [:worker, :supervisor, :admin, :big_boss]
