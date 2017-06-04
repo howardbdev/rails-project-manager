bfc04f292de1e352c7d2307546d828b28f2a1f2a
Howard DeVennish <howarddevennish@gmail.com>
2017-06-04 23:16:43 +0000

    fix session create to make email login case-insensitive

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -5,7 +5,7 @@ class SessionsController < ApplicationController
   end
 
   def create
-    @user = User.find_by(email: user_params[:email])
+    @user = User.where("lower(email) = ?", user_params[:email].downcase).first
     if @user && @user.authenticate(user_params[:password])
       session[:user_id] = @user.id
       redirect_to @user
