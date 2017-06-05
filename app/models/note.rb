a6b6a8d61c7002481e0b1ad9d302e1bd709b5726
Howard DeVennish <howarddevennish@gmail.com>
2017-06-05 22:22:48 +0000

    add note display to project show page

--- app/models/note.rb
+++ app/models/note.rb
@@ -4,12 +4,15 @@ class Note < ApplicationRecord
   validates :content, presence: true
 
   def time_created
-    if message.created_at > Time.now.beginning_of_day
-      "#{time_ago_in_words(message.created_at)} ago"
+    if self.created_at > Time.now.beginning_of_day
+      "#{time_ago_in_words(self.created_at)} ago"
     else
-        <%= message.created_at.strftime("%b %d, %Y")
+      self.created_at.strftime("%b %d, %Y")
     end
+  end
 
+  def author
+    User.where(id: self.user_id).first
   end
 
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,11 +10,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170528170037) do
+ActiveRecord::Schema.define(version: 20170605211450) do
 
   create_table "assignments", force: :cascade do |t|
     t.integer  "user_id"
     t.integer  "project_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "notes", force: :cascade do |t|
+    t.string   "content"
+    t.integer  "project_id"
+    t.integer  "user_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
