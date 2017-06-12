e08fc305d2d4ac739537bc8cfcacfbfa41fd1f01
Howard DeVennish <howarddevennish@gmail.com>
2017-06-12 21:31:02 +0000

    add ability to delete tool

--- app/controllers/tools_controller.rb
+++ app/controllers/tools_controller.rb
@@ -11,4 +11,15 @@ class ToolsController < ApplicationController
   def show
     @tool = Tool.find_by(id: params[:id])
   end
+
+  def destroy
+    @tool = Tool.find_by(id: params[:id])
+    if @tool.delete
+      flash[:notice] = "Tool successfully deleted."
+    else
+      flash[:alert] = "There was an error processing that request."
+      flash[:error] = @tool.errors.full_messages.to_sentence if @tool.errors.any?
+    end
+    redirect_to projects_path
+  end
 end
