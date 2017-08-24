ead0e3f74feb8780deaadb26c7901bd2d5532a3b
Howard DeVennish <howarddevennish@gmail.com>
2017-08-24 15:03:03 -0400

    well now I have no idea what's going on

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -10,10 +10,10 @@
 // Read Sprockets README (https://github.com/rails/sprockets#sprockets-directives) for details
 // about supported directives.
 //
+//= require handlebars.min
 //= require jquery
 //= require jquery_ujs
-//= require_tree .
 //= require jquery3
 //= require bootstrap-sprockets
 //= require turbolinks
-//= require handlebars.min
+//= require_tree .
