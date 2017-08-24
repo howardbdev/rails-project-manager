723f58603e4d30bb6a4469bb9d67d8a020ea45f5
Howard DeVennish <howarddevennish@gmail.com>
2017-08-24 16:12:54 -0400

    stub out basic ajax call for quick-view of project show

--- app/assets/javascripts/projects.js
+++ app/assets/javascripts/projects.js
@@ -1,10 +1,12 @@
 $(function() {
   Project.ready();
 })
-//just trying out atom's commit tool
+
 Project.ready = function() {
   Project.templateSource = $("#project-template").html();
   Project.template = Handlebars.compile(Project.templateSource);
+  $(document).on("submit", ".project-quick-view", Project.showProject);
+  // $(document).on("submit", ".delete-note", Project.deleteNote);
 
 }
 
@@ -17,4 +19,34 @@ function Project(attrs) {
   this.due_date = attrs.due_date;
   this.created_at = attrs.created_at;
   this.updated_at = attrs.updated_at;
+}
+
+// Project.success = function(json) {
+//   var proj = new Project(json);
+//   var noteLI = note.renderLI();
+//   $("#notes").append(noteLI);
+//   $("#note_content").val("");
+// }
+
+Project.showProject = function(e) {
+    e.preventDefault();
+    const action = this.action;
+    // const params = $(this).serialize();
+
+    $.ajax({
+      type: "GET",
+      url: action,
+      success: (response) => {
+        console.log("SUCCESS");
+      },
+      error: (response) => {console.log("FAILURE")},
+      dataType: "json",
+    })
+    // $.get(action, () => {}, "json")
+    //   .done(console.log("SUCCESS"))
+    //   .fail(console.log("FAILURE"))
+      // .done(Project.success)
+      // .always(Project.enableSubmitButton)
+      // .fail(Project.error)
+      // .always(Project.enableSubmitButton)
 }
