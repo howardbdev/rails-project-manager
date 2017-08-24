d39bdca2276eefe3d46780b4d61db676082cce5e
Howard DeVennish <howarddevennish@gmail.com>
2017-08-24 14:01:04 -0400

    begin to stub out js models for user and project

---
+++ app/assets/javascripts/projects.js
@@ -0,0 +1,10 @@
+function Project(attrs) {
+  this.id = attrs.id;
+  this.description = attrs.description;
+  this.location = attrs.location;
+  this.status = attrs.status;
+  this.owner_id = attrs.owner_id;
+  this.due_date = attrs.due_date;
+  this.created_at = attrs.created_at;
+  this.updated_at = attrs.updated_at;
+}
---
+++ app/assets/javascripts/users.js
@@ -0,0 +1,6 @@
+function User(attrs) {
+  this.id = attrs.id;
+  this.email = attrs.email;
+  this.role = attrs.role;
+  this.created_at = attrs.created_at;
+}
